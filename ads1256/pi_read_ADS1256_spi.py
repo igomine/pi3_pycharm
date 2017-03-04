5202d8ce2f473299a23fa63f58d6d806be55ff06
igomine <igomine@126.com>
2017-03-04 23:15:58 +0000

    success read AIN0 val

--- ads1256/pi_read_ADS1256_spi.py
+++ ads1256/pi_read_ADS1256_spi.py
@@ -1,6 +1,7 @@
-import pyads1256
 import sys
 import time
+
+from ads1256 import pyads1256
 
 ads = pyads1256.ADS1256()
 ads.init()
@@ -14,13 +15,20 @@ try:
         ads.SetInputMux(ads.MUX_AIN0, ads.MUX_AINCOM)
         time.sleep(0.2)  # Multiple of line frequency period
         val = ads.ReadADC()
-        sys.stdout.write("AIN_0 value: {:d}\n".format(val))
+        sys.stdout.write("AIN_0 value: {:d} -- ".format(val))
+
+        # print Voltage
+        vtemp = val*100/167
+        print("Voltage: %d.%d %d V" % (vtemp/1000000, vtemp%1000000/1000, vtemp%1000))
 
         # ads.SetInputMux(ads.MUX_AIN1, ads.MUX_AINCOM)
         # time.sleep(0.2)
         # val = ads.ReadADC()
-        # sys.stdout.write("AIN_1 value: {:d}\r\033M".format(val))
+        # sys.stdout.write("AIN_1 value: {:d}\n".format(val))
         # sys.stdout.flush()
+
+        time.sleep(0.3)
+
 except KeyboardInterrupt:
     print("exit")
     ads.spi_close()
