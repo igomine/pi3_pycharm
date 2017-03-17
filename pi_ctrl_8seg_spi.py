a28d373992de17a6e5a7cf622bf5b9efb2c4986f
igomine <igomine@126.com>
2017-03-17 09:34:08 +0000

    1

---
+++ pi_ctrl_8seg_spi.py
@@ -0,0 +1,27 @@
+import spidev
+import time
+
+
+bus = 0
+device = 0
+spi = spidev.SpiDev(bus, device)
+spi.open(0, 0)
+
+# print(spi.mode)
+# print(spi.threewire)
+# print(spi.cshigh)
+# print(spi.bits_per_word)
+# print(spi.lsbfirst)
+spi.mode = 0b11
+spi.max_speed_hz = 50
+spi.cshigh = False
+
+spi_send = [0xff, 0xa4]
+
+
+try:
+    while True:
+        resp = spi.xfer(spi_send)
+except KeyboardInterrupt:
+    print("exit")
+    spi.close()
